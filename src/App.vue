c55cabe5a4ece886c7b2f05f29e38307ffd55c49
Adam Durrant <adam@Adams-MacBook-Pro.local>
2024-01-06 16:27:54 +0000

    More styling, vali dation and suggestions feature

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,23 @@ import { debounce } from 'lodash';
 const searchVal = ref('');
 const searchResults = ref([]);
 const loading = ref(false);
-let searchTimeout;
+var suggestions = document.getElementById('suggestions');
+
+function suggestionQuery(suggestion) {
+  const mainInput = document.getElementById('main-input');
+  mainInput.value = suggestion;
+
+  // Manually trigger the input event
+  const inputEvent = new Event('input', { bubbles: true });
+  mainInput.dispatchEvent(inputEvent);
+
+  // Call handleSearch directly if needed
+  handleSearch();
+}
+
+const handleSuggestionClick = (suggestion) => {
+  suggestionQuery(suggestion);
+}
 
 async function getAcronyms(query) {
   const { data } = await supabase.from('acronyms').select().ilike('acronym', `%${query}%`)
@@ -18,7 +34,7 @@ async function getAcronyms(query) {
 const handleSearch = debounce(() => {
   const trimmedQuery = searchVal.value.trim();
 
-  if (trimmedQuery === '' || !/^[a-zA-Z0-9 ]+$/.test(trimmedQuery)) {
+  if (trimmedQuery === '' || !/^[a-zA-Z0-9 ]+$/.test(trimmedQuery) || trimmedQuery.length == 1) {
     searchResults.value = [];
     return;
   }
@@ -33,32 +49,49 @@ const handleSearch = debounce(() => {
 </script>
 
 <template>
-
   <div class="main-wrapper">
 
-  <section class="intro">
-    <h1>WoW Abbreviations & Acronyms Lookup</h1>
-  </section>
+    <section class="intro">
+      <h1>WoW Abbreviations Lookup</h1>
+    </section>
 
-  <section class="search">
-    <div class="input-wrapper">
-      <SearchInput @input="handleSearch" v-model="searchVal" />
-    </div>
-  </section>
+    <section class="search">
+      <div class="input-wrapper">
+        <SearchInput id="main-input" @input="handleSearch" v-model="searchVal" :type="'search'" :modelValue="''"
+          :wrapperClass="'search-input-wrapper'" :searchIcon="false" :shortcutIcon="true" :clearIcon="true"
+          :hideShortcutIconOnBlur="false" :clearOnEsc="false" :blurOnEsc="true" :selectOnFocus="false"
+          :shortcutListenerEnabled="true" :shortcutKey="'/'" />
+      </div>
+      <div id="suggestions" class="link-list">
+        <h3>Most searched:</h3>
+        <a class="suggestion" @click="() => handleSuggestionClick('AQ40')" href="#">AQ40</a>
+        <a class="suggestion" @click="() => handleSuggestionClick('RDF')" href="#">RDF</a>
+        <a class="suggestion" @click="() => handleSuggestionClick('OOM')" href="#">OOM</a>
+        <a class="suggestion" @click="() => handleSuggestionClick('AoE')" href="#">AoE</a>
+        <a class="suggestion" @click="() => handleSuggestionClick('BoP')" href="#">BoP</a>
+        <a class="suggestion" @click="() => handleSuggestionClick('PUG')" href="#">PUG</a>
+      </div>
+    </section>
 
-  <section class="results">
-    <p v-if="loading">Searching...</p>
-    <ul v-else-if="searchVal !== '' && searchResults.length > 0">
-      <li v-for="result in searchResults" :key="result.id">
-        {{ result.acronym }} - {{ result.meaning }}
-      </li>
-    </ul>
-    <p v-else-if="searchVal !== ''">No results found.</p>
-    <p v-else>Enter a search query.</p>
-  </section>
+    <section class="results">
+      <transition name="fade" mode="out-in">
+      <p v-if="loading">Searching...</p>
 
-</div>
+        <ul class="result-list" v-else-if="searchVal !== '' && searchResults.length > 0">
+          <li class="result-item" v-for="result in searchResults" :key="result.id">
+            <span class="acronym">{{ result.acronym }}</span><span> &nbsp;👉&nbsp;  </span>{{ result.meaning }}
+          </li>
+        </ul>
 
+      <p v-else-if="searchVal !== ''">No results found.</p>
+      <p v-else>Enter a search query.</p>
+    </transition>
+    </section>
+
+  </div>
+  <footer>
+    <p class="footer-text">Made by <a href="https://adamdurrant.co.uk/">Adam Durrant</a></p>
+  </footer>
 </template>
 
 
@@ -71,8 +104,4 @@ export default {
 </script>
 
 <style scoped>
-
-
-
-
 </style>
